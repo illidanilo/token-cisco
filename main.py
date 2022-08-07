7fc67dfe0a82343e6ccce8b9aee95df3d5322c81
danilo <solo2danny@gmail.com>
2022-08-07 19:05:50 -0400

    token final

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import requests
 import json
+###login credentials admin and password : !v3G@!4@Y and is located at
+####https://sandboxapicdc.cisco.com
+
+def obtener_token(usuario, clave):
 
 
-url = "https://10.10.20.14/api/aaaLogin.json"
-body = {
-    "aaaUser":{
-        "attributes":{
-            "name":"admin",
-            "pwd":"C1sco12345"
+    url = "https://sandboxapicdc.cisco.com/api/aaaLogin.json"
+    body = {
+        "aaaUser":{
+            "attributes":{
+                "name":usuario,
+                "pwd":clave
+
+            }
         }
     }
-}
-cabecera = {
-"Content-Type": "application/json"
-}
-requests.packages.urllib3.disable_warnings()
-#requests.packages.urllib3.disable_warnings() : deshabilita las advertencias
-#verify=False: omite la verificacion
-respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
-token = respuesta.json()["imdata"][0]["aaaLogin"]["attributes"]["token"]
-
-
+    cabecera = {
+    "Content-Type": "application/json"
+    }
+    requests.packages.urllib3.disable_warnings()
+    #requests.packages.urllib3.disable_warnings() : deshabilita las advertencias
+    #verify=False: omite la verificacion
+    respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
+    token = respuesta.json()["imdata"][0]["aaaLogin"]["attributes"]["token"]
+    return token
+token = obtener_token("admin", "!v3G@!4@Y")
 print(token)
