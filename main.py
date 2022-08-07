6851027a9bfbf6e5c1da849dbdef850ab087be70
danilo <solo2danny@gmail.com>
2022-08-07 14:57:42 -0400

    nuevos errores

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-url = "https://10.10.20.14/api/aalogin.json"
+url = "https://10.10.20.14/api/aaaLogin.json"
 body = {
-    "aaaUSer":{
+    "aaaUser":{
         "attributes":{
             "name":"admin",
             "pwd":"Cisco12345"
@@ -11,7 +11,11 @@ body = {
     }
 }
 cabecera = {
-    "Content.Type":"application/json"
+    "Content-Type": "application/json"
 }
-requests = requests.post(url, headers=cabecera, data=body, verify=False)
+requests.packages.urllib3.disable_warnings()
+#requests.packages.urllib3.disable_warnings() : deshabilita las advertencias
+#verify=False: omite la verificacion
+requests = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
 
+x=1
